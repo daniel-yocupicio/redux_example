cf3d0efb99dbec715afd131a512d05acabad3f0e
unknown <dag_98@hotmail.com>
2022-12-26 19:43:02 -0600

    add pages

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Header from './components/Header';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Products from './pages/Products';
 import NewProduct from './pages/NewProduct';
 import EditProduct from './pages/EditProduct';
@@ -11,11 +11,11 @@ function App() {
       <Header />
 
       <div className="container mt-5">
-        <Switch>
-          <Route exact path="/" component={Products}/>
-          <Route exact path="/productos/nuevo" component={NewProduct}/>
-          <Route exact path="/productos/editar/:id" component={EditProduct}/>
-        </Switch>
+        <Routes>
+          <Route exact path="/" element={<Products />}/>
+          <Route exact path="/productos/nuevo" element={<NewProduct />}/>
+          <Route exact path="/productos/editar/:id" element={<EditProduct />}/>
+        </Routes>
       </div>
     </Router>
   );
