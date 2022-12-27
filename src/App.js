76eceee1ca816dd07b427ad43cbe89b1f1b2438d
unknown <dag_98@hotmail.com>
2022-12-26 19:24:04 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Header from './components/Header';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Products from './pages/Products';
+import NewProduct from './pages/NewProduct';
+import EditProduct from './pages/EditProduct';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Header />
+
+      <div className="container mt-5">
+        <Switch>
+          <Route exact path="/" component={Products}/>
+          <Route exact path="/productos/nuevo" component={NewProduct}/>
+          <Route exact path="/productos/editar/:id" component={EditProduct}/>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/pages/NewProduct.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const NewProduct = () => {
+    return (
+      <h1>Nuevo producto</h1>
+    );
+}
+
+export default NewProduct;
